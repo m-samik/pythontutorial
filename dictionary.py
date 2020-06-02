fec617d748769b74f3c489cd28cc30d60b9fd77b
Muhammad Sami <61789670+iammsami@users.noreply.github.com>
2020-06-02 23:10:21 +0530

    Dictionary Working with Python

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+#creating a dictionary with words and meanings
+d1 ={"Mutable":"Which Changes","Immutable" : "Which doesn't Change","Luminous":"Object with Light","Sonorous" : "Sound Producing Utencils"}
+print("Enter the word to which you want meaning")
+print(d1[input()])
